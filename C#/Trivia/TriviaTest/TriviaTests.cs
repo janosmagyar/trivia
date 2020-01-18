f6ee499059bc29ef4ff9c09eb73eb9ea77d933d1
János Magyar <zmmaja@gmail.com>
2020-01-18 10:33:04 +0100

    t use Aproovals

--- C#/Trivia/TriviaTest/TriviaTests.cs
+++ C#/Trivia/TriviaTest/TriviaTests.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 using Trivia;
 using Xunit;
 
 namespace TriviaTest
 {
     
+    [UseReporter(typeof(DiffReporter))]
     public class TriviaTests
     {
-        [Fact(Skip = "Only at first time")]
-        public void GenerateFacts()
+        [Fact]
+        public void CheckFacts()
         {
-            using (var output = new StreamWriter("testoutput.txt"))
+            using (var output = new StringWriter())
             {
                 Console.SetOut(output);
                 for (int seed = 0; seed < 1000; seed++)
                 {
                     GameRunner.Main(new string[] {seed.ToString()});
                 }
+                Approvals.Verify(output);
             }
         }
     }
