81f80d671cf7a6134dc5e8da3ea1b8c72b090fd1
János Magyar <zmmaja@gmail.com>
2020-01-18 10:13:09 +0100

    e xunit test

--- C#/Trivia/TriviaTest/Class1.cs
+++ C#/Trivia/TriviaTest/Class1.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using Xunit;
 
 namespace TriviaTest
 {
+    
     public class Class1
     {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(false);
+        }
     }
 }
